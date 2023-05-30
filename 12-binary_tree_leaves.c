7290782474e407d41d04680c81248925723e85f9
Omachonu Prosper <omachonucodes@gmail.com>
2023-05-30 07:03:46 -0700

    Leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: The number of leaves in the binary tree
+ * 0 if tree in NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t left_leaves, right_leaves;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+	
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
+	return (left_leaves + right_leaves);
+}
