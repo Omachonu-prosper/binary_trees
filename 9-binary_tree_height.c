9415448d0597fb89db00b2569fa62c67cada3e21
Omachonu Prosper <omachonucodes@gmail.com>
2023-05-30 05:38:02 -0700

    Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height
+ *
+ * Return: The height of the binary tree
+ * 0 if tree in NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->right && !tree->left)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height < right_height)
+		return (left_height);
+	else
+		return (right_height);
+}
