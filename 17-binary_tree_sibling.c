efcadc4b1938ef82eaebf87d2ccd283f07a503dd
Omachonu Prosper <omachonucodes@gmail.com>
2023-05-31 08:31:39 -0700

    Sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ *
+ * Return: a pointer to the sibling node
+ * NULL otherwise
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	binary_tree_t *parent = node->parent;
+
+	if (node == parent->left && parent->right)
+		return (parent->right);
+	else if (node == parent->right && parent->left)
+		return (parent->left);
+	else
+		return (NULL);
+}
