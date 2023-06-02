c50bbd3bb2ae1d26ff4dc962a886aee4d5f11d57
Omachonu Prosper <omachonucodes@gmail.com>
2023-06-02 15:46:45 -0700

    Fix - test if node is null before calling its parent

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,10 +9,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
 	if (!node || !node->parent)
 		return (NULL);
+
+	parent = node->parent;
 
 	if (node == parent->left && parent->right)
 		return (parent->right);
